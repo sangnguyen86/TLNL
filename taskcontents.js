48ddda1a393efed523bbd0ea113e1775d9f13791
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-20 15:31:49 +0700

    Update taskcontents.js

--- taskcontents.js
+++ taskcontents.js
@@ -1,1 +1,103 @@
 
+// KHCV//
+
+const  task_01_priority = null;//
+const  task_01_mach =  =  "PHÒNG ĐÀO TẠO(tên cũ: HỘI TRƯỜNG)";
+const  task_01_content  ="***";
+const  task_01_start  ="***";
+const  task_01_end ="***";
+const  task_01_design ="***";
+const  task_01_assign ="***";
+const  task_01_support ="***";
+const  task_01_note ="***";
+
+const  task_02_priority = null;//
+const  task_02_mach =  ="***";
+const  task_02_content  ="***";
+const  task_02_start  ="***";
+const  task_02_end ="***";
+const  task_02_design ="***";
+const  task_02_assign ="***";
+const  task_02_support ="***";
+const  task_02_note ="***";
+
+const  task_03_priority = null;//
+const  task_03_mach =  ="***";
+const  task_03_content  ="***";
+const  task_03_start  ="***";
+const  task_03_end ="***";
+const  task_03_design ="***";
+const  task_03_assign ="***";
+const  task_03_support ="***";
+const  task_03_note ="***";
+
+const  task_04_priority = null;//
+const  task_04_mach =  ="***";
+const  task_04_content  ="***";
+const  task_04_start  ="***";
+const  task_04_end ="***";
+const  task_04_design ="***";
+const  task_04_assign ="***";
+const  task_04_support ="***";
+const  task_04_note ="***";
+
+const  task_05_priority = null;//
+const  task_05_mach =  ="***";
+const  task_05_content  ="***";
+const  task_05_start  ="***";
+const  task_05_end ="***";
+const  task_05_design ="***";
+const  task_05_assign ="***";
+const  task_05_support ="***";
+const  task_05_note ="***";
+
+const  task_06_priority = null;//
+const  task_06_mach =  ="***";
+const  task_06_content  ="***";
+const  task_06_start  ="***";
+const  task_06_end ="***";
+const  task_06_design ="***";
+const  task_06_assign ="***";
+const  task_06_support ="***";
+const  task_06_note ="***";
+
+const  task_07_priority = null;//
+const  task_07_mach =  ="***";
+const  task_07_content  ="***";
+const  task_07_start  ="***";
+const  task_07_end ="***";
+const  task_07_design ="***";
+const  task_07_assign ="***";
+const  task_07_support ="***";
+const  task_07_note ="***";
+
+const  task_08_priority = null;//
+const  task_08_mach =  ="***";
+const  task_08_content  ="***";
+const  task_08_start  ="***";
+const  task_08_end ="***";
+const  task_08_design ="***";
+const  task_08_assign ="***";
+const  task_08_support ="***";
+const  task_08_note ="***";
+
+const  task_09_priority = null;//
+const  task_09_mach =  ="***";
+const  task_09_content  ="***";
+const  task_09_start  ="***";
+const  task_09_end ="***";
+const  task_09_design ="***";
+const  task_09_assign ="***";
+const  task_09_support ="***";
+const  task_09_note ="***";
+
+const  task_10_priority = null;//
+const  task_10_mach =  ="***";
+const  task_10_content  ="***";
+const  task_10_start  ="***";
+const  task_10_end ="***";
+const  task_10_design ="***";
+const  task_10_assign ="***";
+const  task_10_support ="***";
+const  task_10_note ="***";
+
