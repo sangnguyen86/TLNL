311e314efaeca6d41283dc3eb8f0fffddc7eed98
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-20 14:53:24 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -1,7 +1,10 @@
 //GLOBAL VAR
 // MESSAGE//
-var myMessage01 = "CHỦ ĐỘNG - SÁNG TẠO - HIỆU QUẢ";
+const myMessage01 = "CHỦ ĐỘNG - SÁNG TẠO - HIỆU QUẢ";
 var myMessage02 = null;
 var myMessage03 = null;
 var myMessage04 = null;
 var myMessage05 = null;
+
+const  task_01_priority = null;//
+const  task_01_mach =  =  "PHÒNG ĐÀO TẠO <br>(tên cũ: HỘI TRƯỜNG)";
