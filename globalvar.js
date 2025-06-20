ede7144e204fd573ac008b3069278abceebfba4a
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-20 11:29:55 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -1,6 +1,6 @@
 //GLOBAL VAR
 // MESSAGE//
-var myMessage01 = "14h30 Ngày 20/06/2025  ";
+var myMessage01 = "14h30 Ngày 20/06/2025";
 var myMessage02 = null;
 var myMessage03 = null;
 var myMessage04 = null;
