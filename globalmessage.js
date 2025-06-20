423e8652351f58a1d355883ec7345c6932e1ed66
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-20 10:08:42 +0700

    Update globalmessage.js

--- globalmessage.js
+++ globalmessage.js
@@ -1,1 +1,6 @@
+/* MESSGAGE */
 
+const msg01_element = document.getElementById("message01");
+msg01_element.innerHTML = "THÔNG BÁO 01";
+const msg02_element = document.getElementById("message02");
+msg02_element.innerHTML = "THÔNG BÁO 02";
