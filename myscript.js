d90e23c5057b4491747bcd63520deeedabc1c13a
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-20 10:05:14 +0700

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -39,4 +39,8 @@ function renderMessage() {
 
 renderMessage();
 /* MESSGAGE */
-document.getElementById("message01").textContent="newtext";
+
+const msg01_element = document.getElementById("message01");
+msg01_element.innerHTML = "THÔNG BÁO 01";
+const msg02_element = document.getElementById("message02");
+msg02_element.innerHTML = "THÔNG BÁO 02";
