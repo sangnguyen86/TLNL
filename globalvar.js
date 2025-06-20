8e777cfcccd2c5931e698079185d7786243ce583
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-20 16:29:46 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -7,3 +7,4 @@ let myMessage04 = null;
 let myMessage05 = null;
 
 const  task_01_mach =  "P. Đào Tạo";
+const  task01_content =  "- Làm theo thứ tự sau.<br> 1. Gắn 6 thêm bóng âm trần.<br>2. Thay bóng đèn 1m2.<br>3. Thay bóng đèn WC<br>4. Gắn máy sấy tay ở 2 WC<br>5. Gắn thêm CB, ổ cắm cho các vị trí lắp đặt các máy khác";
