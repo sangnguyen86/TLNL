c900a8aef7f9be7f037f91420d948b6fe8b5270c
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-20 15:41:42 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -1,7 +1,8 @@
 //GLOBAL VAR
 // MESSAGE//
-var myMessage01 = "CHỦ ĐỘNG - SÁNG TẠO - HIỆU QUẢ";
-var myMessage02 = null;
+let myMessage01 = "CHỦ ĐỘNG - SÁNG TẠO - HIỆU QUẢ";
+let myMessage02;
+myMessage02 = "TIẾT KIỆM";
 var myMessage03 = null;
 var myMessage04 = null;
 var myMessage05 = null;
