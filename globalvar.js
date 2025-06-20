910f79d25ffe1855cb6899d5a3ce2ccdc1513ad1
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-20 14:31:42 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -6,3 +6,104 @@ var myMessage03 = null;
 var myMessage04 = null;
 var myMessage05 = null;
 
+// KHCV//
+
+var task_01_priority = null;//
+var task_01_mach =  ="***";
+var task_01_content  ="***";
+var task_01_start  ="***";
+var task_01_end ="***";
+var task_01_design ="***";
+var task_01_assign ="***";
+var task_01_support ="***";
+var task_01_note ="***";
+
+var task_02_priority = null;//
+var task_02_mach =  ="***";
+var task_02_content  ="***";
+var task_02_start  ="***";
+var task_02_end ="***";
+var task_02_design ="***";
+var task_02_assign ="***";
+var task_02_support ="***";
+var task_02_note ="***";
+
+var task_03_priority = null;//
+var task_03_mach =  ="***";
+var task_03_content  ="***";
+var task_03_start  ="***";
+var task_03_end ="***";
+var task_03_design ="***";
+var task_03_assign ="***";
+var task_03_support ="***";
+var task_03_note ="***";
+
+var task_04_priority = null;//
+var task_04_mach =  ="***";
+var task_04_content  ="***";
+var task_04_start  ="***";
+var task_04_end ="***";
+var task_04_design ="***";
+var task_04_assign ="***";
+var task_04_support ="***";
+var task_04_note ="***";
+
+var task_05_priority = null;//
+var task_05_mach =  ="***";
+var task_05_content  ="***";
+var task_05_start  ="***";
+var task_05_end ="***";
+var task_05_design ="***";
+var task_05_assign ="***";
+var task_05_support ="***";
+var task_05_note ="***";
+
+var task_06_priority = null;//
+var task_06_mach =  ="***";
+var task_06_content  ="***";
+var task_06_start  ="***";
+var task_06_end ="***";
+var task_06_design ="***";
+var task_06_assign ="***";
+var task_06_support ="***";
+var task_06_note ="***";
+
+var task_07_priority = null;//
+var task_07_mach =  ="***";
+var task_07_content  ="***";
+var task_07_start  ="***";
+var task_07_end ="***";
+var task_07_design ="***";
+var task_07_assign ="***";
+var task_07_support ="***";
+var task_07_note ="***";
+
+var task_08_priority = null;//
+var task_08_mach =  ="***";
+var task_08_content  ="***";
+var task_08_start  ="***";
+var task_08_end ="***";
+var task_08_design ="***";
+var task_08_assign ="***";
+var task_08_support ="***";
+var task_08_note ="***";
+
+var task_09_priority = null;//
+var task_09_mach =  ="***";
+var task_09_content  ="***";
+var task_09_start  ="***";
+var task_09_end ="***";
+var task_09_design ="***";
+var task_09_assign ="***";
+var task_09_support ="***";
+var task_09_note ="***";
+
+var task_10_priority = null;//
+var task_10_mach =  ="***";
+var task_10_content  ="***";
+var task_10_start  ="***";
+var task_10_end ="***";
+var task_10_design ="***";
+var task_10_assign ="***";
+var task_10_support ="***";
+var task_10_note ="***";
