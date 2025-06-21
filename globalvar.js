e29925f4fe4fb5cf2f5a25f853c186ecd4d57633
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-21 09:05:53 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -1,6 +1,6 @@
 //GLOBAL VAR
 // MESSAGE//
-let myMessage01 = null;
+let myMessage01 = "MỞ CHỊU NHIỆT: CHỈ DÙNG CHO QUẠT HÚT ỐNG KHÓI VÀ QUẠT HÚT LÒ HƠI";
 let myMessage02 = null;
 let myMessage03 = null;
 let myMessage04 = null;
