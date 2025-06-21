c85e6ae49b1bccd308b1da921a1468c2819988cb
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-21 10:22:15 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -8,111 +8,111 @@ let myMessage05 = null;
 
 // TASK
 // ROW 1
-let  task_01_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_01_mach =  "P. Đào Tạo";
-let  task_01_content =  "- Làm theo thứ tự sau.<br> 1. Gắn 6 thêm bóng âm trần.<br>Thay bóng đèn WC<br>Gắn máy sấy tay ở 2 WC<br>Gắn thêm CB, ổ cắm cho các vị trí lắp đặt các máy khác";
-let  task_01_start  ="20/06/2025";
-let  task_01_end ="24/06/2025";
-let  task_01_assign ="***";
-let  task_01_employ ="Mật Thuỷ <br> Duy Hưng";
-let  task_01_support ="Minh Khánh<br> Văn Hưng";
-let  task_01_notes ="***";
+const  task_01_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_01_mach =  "P. Đào Tạo";
+const  task_01_content =  "- Làm theo thứ tự sau.<br> 1. Gắn 6 thêm bóng âm trần.<br>Thay bóng đèn WC<br>Gắn máy sấy tay ở 2 WC<br>Gắn thêm CB, ổ cắm cho các vị trí lắp đặt các máy khác";
+const  task_01_start  ="20/06/2025";
+const  task_01_end ="24/06/2025";
+const  task_01_assign ="***";
+const  task_01_employ ="Mật Thuỷ <br> Duy Hưng";
+const  task_01_support ="Minh Khánh<br> Văn Hưng";
+const  task_01_notes ="***";
 
 // ROW 2//
-let  task_02_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_02_mach  =  null;
-let  task_02_content =  null;
-let  task_02_start  = null;
-let  task_02_end = null;
-let  task_02_assign = null;
-let  task_02_employ = null;
-let  task_02_support = null;
-let  task_02_notes = null;
+const  task_02_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_02_mach  =  null;
+const  task_02_content =  null;
+const  task_02_start  = null;
+const  task_02_end = null;
+const  task_02_assign = null;
+const  task_02_employ = null;
+const  task_02_support = null;
+const  task_02_notes = null;
 // ROW 3//
-let  task_03_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_03_mach =  null;
-let  task_03_content =   null;
-let  task_03_start  =  null;
-let  task_03_end = null;
-let  task_03_assign = null;
-let  task_03_employ = null;
-let  task_03_support = null;
-let  task_03_notes = null;
+const  task_03_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_03_mach =  null;
+const  task_03_content =   null;
+const  task_03_start  =  null;
+const  task_03_end = null;
+const  task_03_assign = null;
+const  task_03_employ = null;
+const  task_03_support = null;
+const  task_03_notes = null;
 // ROW 4//
-let  task_04_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_04_mach =  null;
-let  task_04_content =   null;
-let  task_04_start  =  null;
-let  task_04_end = null;
-let  task_04_assign = null;
-let  task_04_employ = null;
-let  task_04_support = null;
-let  task_04_notes = null;
+const  task_04_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_04_mach =  null;
+const  task_04_content =   null;
+const  task_04_start  =  null;
+const  task_04_end = null;
+const  task_04_assign = null;
+const  task_04_employ = null;
+const  task_04_support = null;
+const  task_04_notes = null;
 
 // ROW 5//
-let  task_05_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_05_mach =  null;
-let  task_05_content =   null;
-let  task_05_start  =  null;
-let  task_05_end = null;
-let  task_05_assign = null;
-let  task_05_employ = null;
-let  task_05_support = null;
-let  task_05_notes = null;
+const  task_05_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_05_mach =  null;
+const  task_05_content =   null;
+const  task_05_start  =  null;
+const  task_05_end = null;
+const  task_05_assign = null;
+const  task_05_employ = null;
+const  task_05_support = null;
+const  task_05_notes = null;
 
 // ROW 6//
-let  task_06_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_06_mach =  null;
-let  task_06_content =   null;
-let  task_06_start  =  null;
-let  task_06_end = null;
-let  task_06_assign = null;
-let  task_06_employ = null;
-let  task_06_support = null;
-let  task_06_notes = null;
+const  task_06_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_06_mach =  null;
+const  task_06_content =   null;
+const  task_06_start  =  null;
+const  task_06_end = null;
+const  task_06_assign = null;
+const  task_06_employ = null;
+const  task_06_support = null;
+const  task_06_notes = null;
 
 // ROW 7//
-let  task_07_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_07_mach =  null;
-let  task_07_content =   null;
-let  task_07_start  =  null;
-let  task_07_end = null;
-let  task_07_assign = null;
-let  task_07_employ = null;
-let  task_07_support = null;
-let  task_07_notes = null;
+const  task_07_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_07_mach =  null;
+const  task_07_content =   null;
+const  task_07_start  =  null;
+const  task_07_end = null;
+const  task_07_assign = null;
+const  task_07_employ = null;
+const  task_07_support = null;
+const  task_07_notes = null;
 
 // ROW 8//
-let  task_08_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_08_mach =  null;
-let  task_08_content =   null;
-let  task_08_start  =  null;
-let  task_08_end = null;
-let  task_08_assign = null;
-let  task_08_employ = null;
-let  task_08_support = null;
-let  task_08_notes = null;
+const  task_08_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_08_mach =  null;
+const  task_08_content =   null;
+const  task_08_start  =  null;
+const  task_08_end = null;
+const  task_08_assign = null;
+const  task_08_employ = null;
+const  task_08_support = null;
+const  task_08_notes = null;
 
 // ROW 9//
-let  task_09_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_09_mach =  null;
-let  task_09_content =   null;
-let  task_09_start  =  null;
-let  task_09_end = null;
-let  task_09_assign = null;
-let  task_09_employ = null;
-let  task_09_support = null;
-let  task_09_notes = null;
+const  task_09_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_09_mach =  null;
+const  task_09_content =   null;
+const  task_09_start  =  null;
+const  task_09_end = null;
+const  task_09_assign = null;
+const  task_09_employ = null;
+const  task_09_support = null;
+const  task_09_notes = null;
 
 // ROW 10//
-let  task_03_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-let  task_03_mach =  null;
-let  task_03_content =   null;
-let  task_03_start  =  null;
-let  task_03_end = null;
-let  task_03_assign = null;
-let  task_03_employ = null;
-let  task_03_support = null;
-let  task_03_notes = null;
+const  task_03_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_03_mach =  null;
+const  task_03_content =   null;
+const  task_03_start  =  null;
+const  task_03_end = null;
+const  task_03_assign = null;
+const  task_03_employ = null;
+const  task_03_support = null;
+const  task_03_notes = null;
 
 
