9661e2253c0f864768bf9099c11f9b93dbc3ba23
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-21 11:25:22 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -8,7 +8,7 @@ let myMessage05 = null;
 
 // TASK
 // ROW 1
-let  task01_priotity = 2;//1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
+const  task01_priotity = 1;//1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
 
 const  task01_mach =  "P. Đào Tạo";
 const  task01_content =  "- Làm theo thứ tự sau.<br> 1. Gắn 6 thêm bóng âm trần.<br>Thay bóng đèn WC<br>Gắn máy sấy tay ở 2 WC<br>Gắn thêm CB, ổ cắm cho các vị trí lắp đặt các máy khác";
@@ -19,7 +19,7 @@ const  task01_employ ="Mật Thuỷ <br> Duy Hưng";
 const  task01_support ="Minh Khánh<br> Văn Hưng";
 const  task01_notes ="***";
 // ROW 2//
-let  task02_priotity = 2;//1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
+const  task02_priotity = 2;//1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
 const  task02_mach  =  "Bộ trao đổi nhiệt<br> Máy gia ẩm thân lá 01" ;
 const  task02_content =  "Làm bộ trao đổi nhiệt";
 const  task02_start  = "20/06/2025";
@@ -29,7 +29,7 @@ const  task02_employ = "KT Cơ khí";
 const  task02_support = "-";
 const  task02_notes = "-";
 // ROW 3//
-let  task03_priotity = 1;//0: default 1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
+const  task03_priotity = 1;//0: default 1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
 const  task03_mach  =  "Quạt hút ống khói lò hơi" ;
 const  task03_content =  "Thay buli 100<br>Thay dây curoa";
 const  task03_start  = "Khi có vật tư";
