55a034d509103714239ea7fddd2638c0d4cbea52
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-21 10:44:47 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -8,7 +8,7 @@ let myMessage05 = null;
 
 // TASK
 // ROW 1
-const  task01_priotity = "default"; //"primary"- Xanh dương; "secondary"-Xám ;"success"- Xanh lá; "danger" - Đỏ; "warning"vàng-->
+const  task01_priotity = "danger"; //"primary"- Xanh dương; "secondary"-Xám ;"success"- Xanh lá; "danger" - Đỏ; "warning"vàng-->
 const  task01_mach =  "P. Đào Tạo";
 const  task01_content =  "- Làm theo thứ tự sau.<br> 1. Gắn 6 thêm bóng âm trần.<br>Thay bóng đèn WC<br>Gắn máy sấy tay ở 2 WC<br>Gắn thêm CB, ổ cắm cho các vị trí lắp đặt các máy khác";
 const  task01_start  ="20/06/2025";
@@ -18,7 +18,7 @@ const  task01_employ ="Mật Thuỷ <br> Duy Hưng";
 const  task01_support ="Minh Khánh<br> Văn Hưng";
 const  task01_notes ="***";
 // ROW 2//
-const  task02_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task02_priotity = "warning"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warning-vàng-->
 const  task02_mach  =  "Bộ trao đổi nhiệt<br> Máy gia ẩm thân lá 01" ;
 const  task02_content =  "Làm bộ trao đổi nhiệt";
 const  task02_start  = "20/06/2025";
