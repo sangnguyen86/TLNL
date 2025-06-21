109e81a5dfd811b536089712626e8ad809504701
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-21 10:42:08 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -8,25 +8,25 @@ let myMessage05 = null;
 
 // TASK
 // ROW 1
-const  task_01_priotity = "default"; //"primary"- Xanh dương; "secondary"-Xám ;"success"- Xanh lá; "danger" - Đỏ; "warning"vàng-->
-const  task_01_mach =  "P. Đào Tạo";
-const  task_01_content =  "- Làm theo thứ tự sau.<br> 1. Gắn 6 thêm bóng âm trần.<br>Thay bóng đèn WC<br>Gắn máy sấy tay ở 2 WC<br>Gắn thêm CB, ổ cắm cho các vị trí lắp đặt các máy khác";
-const  task_01_start  ="20/06/2025";
-const  task_01_end ="24/06/2025";
-const  task_01_assign ="***";
-const  task_01_employ ="Mật Thuỷ <br> Duy Hưng";
-const  task_01_support ="Minh Khánh<br> Văn Hưng";
-const  task_01_notes ="***";
+const  task01_priotity = "default"; //"primary"- Xanh dương; "secondary"-Xám ;"success"- Xanh lá; "danger" - Đỏ; "warning"vàng-->
+const  task01_mach =  "P. Đào Tạo";
+const  task01_content =  "- Làm theo thứ tự sau.<br> 1. Gắn 6 thêm bóng âm trần.<br>Thay bóng đèn WC<br>Gắn máy sấy tay ở 2 WC<br>Gắn thêm CB, ổ cắm cho các vị trí lắp đặt các máy khác";
+const  task01_start  ="20/06/2025";
+const  task01_end ="24/06/2025";
+const  task01_assign ="***";
+const  task01_employ ="Mật Thuỷ <br> Duy Hưng";
+const  task01_support ="Minh Khánh<br> Văn Hưng";
+const  task01_notes ="***";
 // ROW 2//
-const  task_02_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
-const  task_02_mach  =  "Bộ trao đổi nhiệt<br> Máy gia ẩm thân lá 01" ;
-const  task_02_content =  "Làm bộ trao đổi nhiệt";
-const  task_02_start  = "20/06/2025";
-const  task_02_end = "28/06/2025";
-const  task_02_assign = "Hoà";
-const  task_02_employ = "KT Cơ khí";
-const  task_02_support = "-";
-const  task_02_notes = "-";
+const  task02_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task02_mach  =  "Bộ trao đổi nhiệt<br> Máy gia ẩm thân lá 01" ;
+const  task02_content =  "Làm bộ trao đổi nhiệt";
+const  task02_start  = "20/06/2025";
+const  task02_end = "28/06/2025";
+const  task02_assign = "Hoà";
+const  task02_employ = "KT Cơ khí";
+const  task02_support = "-";
+const  task02_notes = "-";
 
 
 
