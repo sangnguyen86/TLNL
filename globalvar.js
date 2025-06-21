37c79227c0b46809d61b27da7c8256334a2aa1c7
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-21 10:35:24 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -8,7 +8,7 @@ let myMessage05 = null;
 
 // TASK
 // ROW 1
-const  task_01_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_01_priotity = "default"; //"primary"- Xanh dương; "secondary"-Xám ;"success"- Xanh lá; "danger" - Đỏ; "warning"vàng-->
 const  task_01_mach =  "P. Đào Tạo";
 const  task_01_content =  "- Làm theo thứ tự sau.<br> 1. Gắn 6 thêm bóng âm trần.<br>Thay bóng đèn WC<br>Gắn máy sấy tay ở 2 WC<br>Gắn thêm CB, ổ cắm cho các vị trí lắp đặt các máy khác";
 const  task_01_start  ="20/06/2025";
@@ -17,6 +17,16 @@ const  task_01_assign ="***";
 const  task_01_employ ="Mật Thuỷ <br> Duy Hưng";
 const  task_01_support ="Minh Khánh<br> Văn Hưng";
 const  task_01_notes ="***";
+// ROW 2//
+const  task_02_priotity = "default"; //primary- Xanh dương; secondary-Xám ;success- Xanh lá; danger - Đỏ; warningvàng-->
+const  task_02_mach  =  "Bộ trao đổi nhiệt<br> Máy gia ẩm thân lá 01" ;
+const  task_02_content =  "Làm bộ trao đổi nhiệt";
+const  task_02_start  = "20/06/2025";
+const  task_02_end = "28/06/2025";
+const  task_02_assign = "Hoà";
+const  task_02_employ = "KT Cơ khí";
+const  task_02_support = "-";
+const  task_02_notes = "-";
 
 
 
