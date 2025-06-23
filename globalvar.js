fbe7f4beef31e8dabb95e2af11936a9eaaebc91b
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-23 08:32:37 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -28,7 +28,7 @@ const  task01_employ ="Mật Thuỷ <br> Duy Hưng";
 const  task01_support ="Minh Khánh<br> Văn Hưng";
 const  task01_notes ="***";
 // ROW 2//
-let  task02_priority = "warning";//1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
+let  task02_priority = "danger";//1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
 const  task02_mach  =  "Bộ trao đổi nhiệt<br> Máy gia ẩm thân lá 01" ;
 const  task02_content =  "Làm bộ trao đổi nhiệt";
 const  task02_start  = "20/06/2025";
@@ -38,15 +38,15 @@ const  task02_employ = "KT Cơ khí";
 const  task02_support = "-";
 const  task02_notes = "-";
 // ROW 3//
-let   task03_priority = "danger";//1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
+let   task03_priority = "warning";//1: danger; 2 warning; 3; primary; 4 secondary; 5 success; //
 const  task03_mach  =  "Quạt hút ống khói lò hơi" ;
-const  task03_content =  "Tháo và kiểm tra cánh quạt";
-const  task03_start  = "22/06/2025";
-const  task03_end = "22/06/2025";
+const  task03_content =  "Gắn chốt lavet buli quạt";
+const  task03_start  = "23/06/2025";
+const  task03_end = "23/06/2025";
 const  task03_assign = "_";
-const  task03_employ = "Văn Hưng, Minh Khánh";
-const  task03_support = "Ngọc Hiếu";
-const  task03_notes = "";
+const  task03_employ = "_";
+const  task03_support = "_";
+const  task03_notes = "Theo dõi máy thường xuyên";
 // ROW 4//
 let   task04_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
 const  task04_mach  =  "Phòng đào tạo" ;
