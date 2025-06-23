0d1367f84197b1dd6aa22fa0314e425361424d4d
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-23 11:05:25 +0700

    Update othertask.js

--- othertask.js
+++ othertask.js
@@ -23,8 +23,57 @@ const  othertask02_end = "31/08/2025";
 const  othertask02_notes = "";
 // ROW 3//
 let   othertask03_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
-const  othertask03_employ = "xxx<br>xxx";
-const  othertask03_content = "Phân công vào xưởng";
-const  othertask03_start  = "03/09/2025";
-const  othertask03_end = "30/11/2025";
+const  othertask03_employ = "employ";
+const  othertask03_content =  "task";
+const  othertask03_start  = "start date";
+const  othertask03_end = "end date";
 const  othertask03_notes = "Notes";
+// ROW 4//
+let   othertask04_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask04_employ = "employ";
+const  othertask04_content =  "task";
+const  othertask04_start  = "start date";
+const  othertask04_end = "end date";
+const  othertask04_notes = "Notes";
+// ROW 5//
+let   othertask05_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask05_employ = "employ";
+const  othertask05_content =  "task";
+const  othertask05_start  = "start date";
+const  othertask05_end = "end date";
+const  othertask05_notes = "Notes";
+// ROW 6//
+let   othertask06_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask06_employ = "employ";
+const  othertask06_content =  "task";
+const  othertask06_start  = "start date";
+const  othertask06_end = "end date";
+const  othertask06_notes = "Notes";
+// ROW 7//
+let   othertask07_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask07_employ = "employ";
+const  othertask07_content =  "task";
+const  othertask07_start  = "start date";
+const  othertask07_end = "end date";
+const  othertask07_notes = "Notes";
+// ROW 8//
+let   othertask08_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask08_employ = "employ";
+const  othertask08_content =  "task";
+const  othertask08_start  = "start date";
+const  othertask08_end = "end date";
+const  othertask08_notes = "Notes";
+// ROW 9//
+let   othertask09_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask09_employ = "employ";
+const  othertask09_content =  "task";
+const  othertask09_start  = "start date";
+const  othertask09_end = "end date";
+const  othertask09_notes = "Notes";
+// ROW 10//
+let   othertask10_priority = "warning";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask10_employ = "xxx<br>xxx";
+const  othertask10_content = "Phân công vào xưởng";
+const  othertask10_start  = "03/09/2025";
+const  othertask10_end = "30/11/2025";
+const  othertask10_notes = "Notes";
