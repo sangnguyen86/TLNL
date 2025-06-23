630b24f936e17a1826b408ac4fd23c693fe3a75a
sangnguyen86 <139835751+sangnguyen86@users.noreply.github.com>
2025-06-23 10:33:29 +0700

    Update globalvar.js

--- globalvar.js
+++ globalvar.js
@@ -118,3 +118,20 @@ const  task10_assign = "";
 const  task10_employ = "";
 const  task10_support = "";
 const  task10_notes = "";
+
+
+// PHÂN CÔNG CÔNG VIỆC KHÁC //
+// ROW 0//
+let   othertask00_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask00_employ = "employ";
+const  othertask00_content =  "task";
+const  othertask00_start  = "start date";
+const  othertask00_end = "end date";
+const  othertask00_notes = "Notes";
+// ROW 1//
+let   othertask01_priority = "none";//danger -warning; 3; primary; 4 secondary; 5 success; //
+const  othertask01_employ = "Đức Duy<br>Tấn Khải";
+const  othertask01_content =  "Phân công vào xưởng";
+const  othertask01_start  = "01/06/2025";
+const  othertask01_end = "31/08/2025";
+const  othertask01_notes = "Học vận hành các cụm máy, thay vận hành nghỉ phép.<br>Tham gia cùng vận hành sửa các hư hỏng.<br><b>Nắm bắt chi tiết thiết bị các máy</b><br>Làm việc dưới sự điều động, giám sát của Trưởng ca/ Tổ trưởng";
